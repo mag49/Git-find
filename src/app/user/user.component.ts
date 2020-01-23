83d4a0612fbfc7151b6e2ee960ff01e5f086baad
Ngethe <ciru930@gmail.com>
2020-01-23 15:04:38 +0300

    added the function for finding the user and the repository in the user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UserService} from '../services/user.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user: any;
+  username: string;
+  repos:any;
 
-  constructor() { }
+  constructor(private userService:UserService) {
+  }
+    
+   findUser(){
+     this.userService.updateUser(this.username);
+     this.userService.getUserInfo().subscribe(user =>{
+      console.log(user)
+      this.user = user;
+      
+    });
+    this.userService.getUserRepos().subscribe(repos =>{
+        console.log(repos);
+        this.repos = repos;
+    })
+   }
 
+   
   ngOnInit() {
+    this.userService.updateUser('mag4');
+    this.userService.getUserInfo().subscribe(user =>this.user = user);
+    this.userService.getUserRepos().subscribe(repos =>this.repos = repos);
+      
+      
+
+      
+      
+      
+
   }
 
 }
