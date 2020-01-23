09bd770e8c0db56aba8cdbd8f5e1ffca1a6cf909
Ngethe <ciru930@gmail.com>
2020-01-23 15:48:55 +0300

    styled the container of the html body to have a background clor

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { observable, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SearchService {
+export class RepoService {
+  returnRepo(searchTerm: string) {
+    // throw new Error("Method not implemented.");
+  }
+  private clientid = "af59a886926a3c41b772";
+  private clientsecret = "a345424c81e1eee3ffc3b2509fc644a0736f5e8c";
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { 
+    
+}
+
+  returnrepo(searchTerm:string):Observable<any>{
+    return this.http.get("https://api.github.com/users/" + searchTerm + "/repos?" + this.clientid + this.clientsecret)
+  }
 }
